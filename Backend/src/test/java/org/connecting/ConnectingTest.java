6935612a4dacfa3103e88d1d933dac1bf04e2206
Uboatwaffe <mpiatek1512@gmail.com>
2023-10-14 21:27:50 +0200

    JavaDoc for Backend/Test

--- Backend/src/test/java/org/connecting/ConnectingTest.java
+++ Backend/src/test/java/org/connecting/ConnectingTest.java
@@ -2,6 +2,12 @@ package org.connecting;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+/**
+ * Test
+ * @see Connecting
+ * @author Maciej
+ * @version 0.1
+ */
 class ConnectingTest {
 
     @org.junit.jupiter.api.Test
--- Backend/src/test/java/org/manage/ToStringsTest.java
+++ Backend/src/test/java/org/manage/ToStringsTest.java
@@ -3,7 +3,12 @@ package org.manage;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * Test
+ * @see ToStrings
+ * @author Maciej
+ * @version 0.1
+ */
 class ToStringsTest {
     final ToStrings toStrings = new ToStrings();
 
