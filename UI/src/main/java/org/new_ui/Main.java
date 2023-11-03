e53336dee0564433417e929a5fbd1a936cf146b6
Uboatwaffe <mpiatek1512@gmail.com>
2023-11-03 12:49:27 +0100

    new ui structure

---
+++ UI/src/main/java/org/new_ui/Main.java
@@ -0,0 +1,70 @@
+package org.new_ui;
+
+import org.file.Writing;
+import org.new_ui.swing.MainSwing;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main extends JFrame {
+
+    Writing writing = new Writing();
+
+    private JPanel panel;
+    private JButton swing;
+    private JButton website;
+    private JButton quit;
+    private JLabel welcome;
+    private JLabel types;
+    private JLabel GUI;
+    private JLabel web;
+    private JLabel errors;
+    private JLabel fun;
+
+    Main() {
+        setContentPane(panel);
+        setTitle("Quiz");
+        setSize(530, 250);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        swing.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new MainSwing();
+                setVisible(false);
+            }
+        });
+        website.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        quit.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+    }
+}
---
+++ UI/src/main/java/org/new_ui/swing/tutorial/Tutorial.java
@@ -0,0 +1,36 @@
+package org.new_ui.swing.tutorial;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+
+public class Tutorial extends JFrame {
+    private JButton understood;
+    private JLabel welocme;
+    private JLabel types;
+    private JLabel closed;
+    private JLabel true_or_false;
+    private JLabel date;
+    private JLabel open;
+    private JPanel panel;
+
+    public Tutorial() {
+        setContentPane(panel);
+        setTitle("Tutorial");
+        setSize(440, 230);
+        setDefaultCloseOperation(HIDE_ON_CLOSE);
+        setVisible(true);
+        understood.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispatchEvent(new WindowEvent(Tutorial.this, WindowEvent.WINDOW_CLOSING));
+            }
+        });
+    }
+}
